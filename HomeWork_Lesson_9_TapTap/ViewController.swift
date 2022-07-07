2f549c2dca04d56e0d3ffa75b576c4e22b9addd4
Artem Makarenko <prestigemakar@gmail.com>
2022-07-07 11:09:54 +0300

    Initial Commit

---
+++ HomeWork_Lesson_9_TapTap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_Lesson_9_TapTap
+//
+//  Created by Артем Макаренко on 07.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
